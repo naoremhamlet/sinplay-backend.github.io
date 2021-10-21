ca966521a44682aa553a96d7aa75363e2fe2f008
naoremhamlet <naoremhamlet@gmail.com>
2021-10-21 15:05:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 const express = require('express') 
-const cors = require('cors');
+
 const app = express();
 
 var bodyParser = require('body-parser')
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+
 
 const {conn} = require('./connection')
 const { Contact } = require('./Contact');
