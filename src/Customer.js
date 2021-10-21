77c53ccf20a0153b3a6a171fe6bf3d5cfbb80fa9
naoremhamlet <naoremhamlet@gmail.com>
2021-10-21 21:21:20 +0530

    Update Customer.js

--- src/Customer.js
+++ src/Customer.js
@@ -11,29 +11,29 @@ module.exports = async function(app, conn, urlencodedParser){
 
 async function push(conn, req, res){
     let { data } = req.body;
-    let sql = `SELECT email FROM customer WHERE email='${data.email}'`;
-    let results = await new Promise((resolve, reject) => {
-        conn.query(sql, (err, result) => {
-            if(err) throw(err)
-            resolve(result)
-        })
-    })
+    // let sql = `SELECT email FROM customer WHERE email='${data.email}'`;
+    // let results = await new Promise((resolve, reject) => {
+    //     conn.query(sql, (err, result) => {
+    //         if(err) console.log(err)
+    //         resolve(result)
+    //     })
+    // })
 
-    if(results.length>0){
-        res.send({ success: false })
-        return;
-    }else{
+    // if(results.length>0){
+    //     res.send({ success: false })
+    //     return;
+    // }else{
         let values = `('${data.email}', '${data.company}', '${data.phone}', '${data.name}', '${data.about}')`;
         
-        sql = `INSERT INTO customer (email, company, phone, name, about) VALUES ${values}`;
+        let sql = `INSERT INTO customer (email, company, phone, name, about) VALUES ${values}`;
         let pushdatas = await new Promise((resolve, reject) => {
             conn.query(sql, (err, result) => {
-                if(err) throw(err)
+                if(err) console.log(err)
                 resolve(result)
             })
         })
 
         await sendmail(data.email, '\tThank You for reaching us.\nWe will get back to you at the earliest.\nHR \nSinplay')
         res.send({ success: true })
-    }
+    //}
 }
