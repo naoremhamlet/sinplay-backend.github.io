025e5ee70fb29c8a5745a43722f46f19873a8f1e
naoremhamlet <84091023+naoremhamlet@users.noreply.github.com>
2021-10-24 07:59:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,20 +2,22 @@ const express = require('express')
 const cors = require('cors');
 const app = express();
 
-var bodyParser = require('body-parser')
-app.use(cors());
+const bodyParser = require('body-parser')
 
 const PORT = process.env.PORT || 8000;
 
 const {conn} = require('./connection')
 const { Contact } = require('./api');
 
+
 //midleware
 
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(Contact);
 
+//know whether runnning or not
 app.get('/', (req, res, next) => res.send("sinplay-backend Running"));
 
 app.listen(PORT,e=>console.log('server start'))
