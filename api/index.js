05b78fd43a7e89a7398bc38887dadcaefa501e35
Naorem Hemlet <naoremhamlet@gmail.com>
2022-05-16 00:12:46 +0530

    check

--- api/index.js
+++ api/index.js
@@ -12,6 +12,7 @@ Contact.post('/api/recieve_message', (req, res, next) => {
 Contact.post('/api/set_meeting', (req, res, next) => {
     Meeting(req, res, next, conn)
 })
+//added this
 Contact.post('/api/subscribe_newsletter', (req, res, next) => {
     Newsletter(req, res, next, conn)
 })
